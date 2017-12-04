c1346777b499b5e47444496b64c0b474a5f51df7
Daniel Volnitsky <11webboy>
2017-12-04 15:56:39 +0200

    6
    6

--- src/main/java/tasks/task6_23_11_2017/regexTask/entities/WordFrequencyHelper.java
+++ src/main/java/tasks/task6_23_11_2017/regexTask/entities/WordFrequencyHelper.java
@@ -8,7 +8,9 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 class WordFrequencyHelper {
-
+/**@return связный map, сортированный по частоте вхождения слова
+ * @param initialMap начальный несортированный map
+ * */
     static Map<String, Map<String, Integer>> getMapSortedByWordFrequency(Map<String, Map<String, Integer>> initialMap, String seekWord) {
         WordFrequencyMapComparator comparator = new WordFrequencyMapComparator(seekWord);
         return initialMap.entrySet().stream()
@@ -17,28 +19,42 @@ class WordFrequencyHelper {
                         (oldValue, newValue) -> oldValue, LinkedHashMap::new));
     }
 
+//    public static <K, V extends Comparable<? super V>> Map<K, V>
+//    sortByValue(Map<K, V> map) {
+//        List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+//        Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+//            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+//                return (o1.getValue()).compareTo( o2.getValue() );
+//            }
+//        });
+//
+//        Map<K, V> result = new LinkedHashMap<K, V>();
+//        for (Map.Entry<K, V> entry : list) {
+//            result.put(entry.getKey(), entry.getValue());
+//        }
+//        return result;
+//    }
+
     static void iterateUrlsAndFillWordsFrequencies(List<String> urls, Map<String, Map<String, Integer>> urlWordsMap, byte neededUrlCount) {
         for (int i = 0; i < urls.size() && urlWordsMap.size() <= neededUrlCount; i++) {
             String url = urls.get(i);
             try {
-                System.out.println(String.format("Working with (%s) url...", url));
-
                 String text = new SiteInfoHelper(url).getSiteBodyText();
 
                 Map<String, Integer> wordsWithFrequency = new HashMap<>();
                 WordFrequencyHelper.fillWordsFrequencyMap(wordsWithFrequency, text);
 
                 if (wordsWithFrequency.size() > 0) {
+                    System.out.println(String.format("Working with (%s) url...", url));
+
                     if (!urlWordsMap.containsKey(url)) {
                         urlWordsMap.put(url, wordsWithFrequency);
                     } else {
                         System.err.println("Этот url уже присутствует в списке: " + url);
                     }
-                } else {
-                    System.err.println("Ни одного слова по заданному url.");
                 }
             } catch (IOException ex) {
-                System.out.println("failed to open connection with url: " + url);
+                //place for logging
             }
         }
     }
